85e6f76036984119d53076e6ac993716ad922213
pushkarpm24 <pushkarmorey555@gmail.com>
2020-12-04 09:54:55 +0530

    ADDED: Test Case For Home Page

---
+++ Bookswagon_Automation/src/main/java/Pages/HomePage.java
@@ -0,0 +1,34 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+    public WebDriver driver;
+    public boolean flag;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(id = "ctl00_TopSearch1_txtSearch")
+    WebElement searchBar;
+
+    @FindBy(id = "ctl00_TopSearch1_Button1")
+    WebElement searchButton;
+
+    @FindBy(xpath = "//span[contains(text(),'harry potter')]")
+    WebElement verification;
+
+    public void search_Book(){
+        searchBar.sendKeys("Harry Potter");
+        searchButton.click();
+        if(verification.toString().contains("harry potter")){
+            flag = true;
+        }
+    }
+}
--- Bookswagon_Automation/src/test/java/TestCases/TestClass.java
+++ Bookswagon_Automation/src/test/java/TestCases/TestClass.java
@@ -1,15 +1,23 @@
 package TestCases;
 
+import Pages.HomePage;
 import Pages.LoginPage;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestClass extends BaseClass {
-    @Test
+    @Test(priority = 1)
     public void LoginPageTest()
     {
         LoginPage login = new LoginPage(driver);
         login.AccountLogin();
+    }
+
+    @Test(priority = 2)
+    public void HomePageTest()
+    {
+        HomePage home = new HomePage(driver);
+        home.search_Book();
     }
 
 
